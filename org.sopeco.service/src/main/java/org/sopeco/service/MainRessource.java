2516e4e2fc66dd1fb83453b6078654d389887c56
Peter Merkert <p.merkert@sap.com>
2013-11-29 10:57:27 +0100

    Todo done: Check running on Tomcat Server again. The REST ist now running on Tomcat again.
    TODO: Use Dynamic Web Module 3.0 for automatic creation of the web.xml
    
    
    Change-Id: I8d4f7e75f374da9ac6445212acd66b7525f15194
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/main/java/org/sopeco/service/MainRessource.java
+++ org.sopeco.service/src/main/java/org/sopeco/service/MainRessource.java
@@ -3,6 +3,7 @@ package org.sopeco.service;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/login")
@@ -16,5 +17,19 @@ public class MainRessource {
 		return Response.status(200).entity(output).build();
  
 	}
+	
+  @GET
+  @Produces(MediaType.TEXT_XML)
+  public String sayXMLHello() {
+	  
+    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+    
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public String sayHtmlHello() {
+    return "<html> " + "<title>" + "Hello Jersey" + "</title>" + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+  }
 	
 }
