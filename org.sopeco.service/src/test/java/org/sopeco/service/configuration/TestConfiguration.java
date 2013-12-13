0d29377ce2ee106906672930e1c1ebed10196dbe
Peter Merkert <p.merkert@sap.com>
2013-12-13 13:48:46 +0100

    Wrote "username" in the tests instead of "accountname". This was a misunderstanding of the data.
    As we are testing account creation and existence, the name is an account
    name and not a username.
    
    Change-Id: I2d7a139699fd93eb407302280fdba0a2430324a7
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/test/java/org/sopeco/service/configuration/TestConfiguration.java
+++ org.sopeco.service/src/test/java/org/sopeco/service/configuration/TestConfiguration.java
@@ -6,6 +6,6 @@ public final class TestConfiguration {
 	public static final String PACKAGE_NAME_POJO = "com.sun.jersey.api.json.POJOMappingFeature";
 	public static final String PACKAGE_NAME_REST = "org.sopeco.service.rest";
 
-	public static final String TESTUSERNAME = "testuser";
+	public static final String TESTACCOUNTNAME = "testuser";
 	public static final String TESTPASSWORD = "testpassword";
 }
--- org.sopeco.service/src/test/java/org/sopeco/service/rest/AccountServiceTest.java
+++ org.sopeco.service/src/test/java/org/sopeco/service/rest/AccountServiceTest.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sopeco.service.configuration.TestConfiguration;
 import org.sopeco.service.configuration.ServiceConfiguration;
-import org.sopeco.service.persistence.entities.account.AccountDetails;
 import org.sopeco.service.shared.Message;
 
 import com.sun.jersey.api.client.config.ClientConfig;
@@ -63,19 +62,19 @@ public class AccountServiceTest extends JerseyTest {
 	 */
 	@Test
 	public void createExistingAccount() {
-		String username = TestConfiguration.TESTUSERNAME;
+		String accountname = TestConfiguration.TESTACCOUNTNAME;
 		String password = TestConfiguration.TESTPASSWORD;
 		
 		// just create the account once to be sure it already exists
 		resource().path(ServiceConfiguration.SVC_ACCOUNT)
 				  .path(ServiceConfiguration.SVC_ACCOUNT_CREATE)
-				  .queryParam(ServiceConfiguration.SVC_ACCOUNT_ACCOUNTNAME, username)
+				  .queryParam(ServiceConfiguration.SVC_ACCOUNT_ACCOUNTNAME, accountname)
 				  .queryParam(ServiceConfiguration.SVC_ACCOUNT_PASSWORD, password)
 				  .post(Message.class);
 		
 		Message m = resource().path(ServiceConfiguration.SVC_ACCOUNT)
 						      .path(ServiceConfiguration.SVC_ACCOUNT_CREATE)
-							  .queryParam(ServiceConfiguration.SVC_ACCOUNT_ACCOUNTNAME, username)
+							  .queryParam(ServiceConfiguration.SVC_ACCOUNT_ACCOUNTNAME, accountname)
 							  .queryParam(ServiceConfiguration.SVC_ACCOUNT_PASSWORD, password)
 						      .post(Message.class);
 
@@ -87,52 +86,24 @@ public class AccountServiceTest extends JerseyTest {
 	 */
 	@Test
 	public void checkAccountExistence() {
-		String username = TestConfiguration.TESTUSERNAME;
+		String accountname = TestConfiguration.TESTACCOUNTNAME;
 		String password = TestConfiguration.TESTPASSWORD;
 		
 		// the creation might fail, but we are only interested if afterwards at least one user
 		// with username "testuser" already exists
 		resource().path(ServiceConfiguration.SVC_ACCOUNT)
 				  .path(ServiceConfiguration.SVC_ACCOUNT_CREATE)
-				  .queryParam(ServiceConfiguration.SVC_ACCOUNT_ACCOUNTNAME, username)
+				  .queryParam(ServiceConfiguration.SVC_ACCOUNT_ACCOUNTNAME, accountname)
 				  .queryParam(ServiceConfiguration.SVC_ACCOUNT_PASSWORD, password)
 				  .post(Message.class);
 
 		Boolean b = resource().path(ServiceConfiguration.SVC_ACCOUNT)
 							  .path(ServiceConfiguration.SVC_ACCOUNT_EXISTS)
-							  .queryParam(ServiceConfiguration.SVC_ACCOUNT_ACCOUNTNAME, username)
+							  .queryParam(ServiceConfiguration.SVC_ACCOUNT_ACCOUNTNAME, accountname)
 							  .accept(MediaType.APPLICATION_JSON)
 							  .get(Boolean.class);
 
 		assertEquals(true, b);
-	}
-	
-	/**
-	 * Checks getting AccountInformation for a created account
-	 * 
-	 * Currently there are no account details created, thats why this test it not tested.
-	 * (No @Test annotation).
-	 */
-	
-	public void checkAccountDetails() {
-		String username = TestConfiguration.TESTUSERNAME;
-		String password = TestConfiguration.TESTPASSWORD;
-		
-		// the creation might fail, but we are only interested if afterwards at least one user
-		// with username "testuser" already exists
-		resource().path(ServiceConfiguration.SVC_ACCOUNT)
-				  .path(ServiceConfiguration.SVC_ACCOUNT_CREATE)
-				  .path(username)
-				  .path(password)
-				  .get(Message.class);
-
-		AccountDetails ad = resource().path(ServiceConfiguration.SVC_ACCOUNT)
-									  .path(ServiceConfiguration.SVC_ACCOUNT_INFO)
-									  .path(username)
-									  .accept(MediaType.APPLICATION_JSON)
-									  .get(AccountDetails.class);
-
-		assertEquals(username, ad.getAccountName());
 	}
 	
 }
