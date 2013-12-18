40f4afa5cd545b3e26d6b1a50617efe4a9426a00
Peter Merkert <p.merkert@sap.com>
2013-12-18 11:30:35 +0100

    Removed unused import to PathParams.
    Change-Id: Ie4b6914fb25caef1350400da83cc97b2dd71651a
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/main/java/org/sopeco/service/rest/AccountService.java
+++ org.sopeco.service/src/main/java/org/sopeco/service/rest/AccountService.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
