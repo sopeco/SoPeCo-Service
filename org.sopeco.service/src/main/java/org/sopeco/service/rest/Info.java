34a62d6b7e90a739c817dcd714e32d042b4b6dd5
Peter Merkert <p.merkert@sap.com>
2014-01-10 17:07:53 +0100

    Added a simple tester to test the information service.
    Motto: Better test simple rather than no testing.
    
    Change-Id: I6c7d52b25c477d7d05d4686a90d093e2a39e4c33
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/main/java/org/sopeco/service/rest/Info.java
+++ org.sopeco.service/src/main/java/org/sopeco/service/rest/Info.java
@@ -5,25 +5,32 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.sopeco.service.configuration.ServiceConfiguration;
 
 @Path(ServiceConfiguration.SVC_INFO)
 public class Info {
-	
-	@SuppressWarnings("unused")
-	private static final Logger LOGGER = LoggerFactory.getLogger(Info.class);
 
 	/**
 	 * Prints information about the SoPeCo Service
-	 * @return
+	 * 
+	 * @return information text about this service
 	 */
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String createAccount() {
 		return "<h1>Information about SoPeCo Service</h1>"
 				+ "Services can be requested.";
+	}
+	
+	/**
+	 * Returns true, if the service is running.
+	 * 
+	 * @return true, if the service is running
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Boolean running() {
+		return true;
 	}
 	
 }
---
+++ org.sopeco.service/src/test/java/org/sopeco/service/rest/InfoServiceTest.java
@@ -0,0 +1,56 @@
+package org.sopeco.service.rest;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.junit.Test;
+import org.sopeco.service.configuration.ServiceConfiguration;
+import org.sopeco.service.configuration.TestConfiguration;
+
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.test.framework.JerseyTest;
+import com.sun.jersey.test.framework.WebAppDescriptor;
+
+public class InfoServiceTest extends JerseyTest {
+
+	public InfoServiceTest() {
+		super();
+	}
+	
+	@Override
+	public WebAppDescriptor configure() {
+		return new WebAppDescriptor.Builder(new String[] {
+				TestConfiguration.PACKAGE_NAME_JSON,
+				TestConfiguration.PACKAGE_NAME_REST })
+				.initParam(TestConfiguration.PACKAGE_NAME_POJO, "true")
+				.clientConfig(createClientConfig())
+				.build();
+	}
+
+	/**
+	 * Sets the client config for the client to accept JSON and
+	 * converting JSON Object to POJOs.
+	 * This method is called by {@link configure()}.
+	 * 
+	 * @return ClientConfig to work with JSON
+	 */
+	private static ClientConfig createClientConfig() {
+		ClientConfig config = new DefaultClientConfig();
+	    config.getClasses().add(JacksonJsonProvider.class);
+	    config.getFeatures().put(TestConfiguration.PACKAGE_NAME_POJO, Boolean.TRUE);
+	    return config;
+	}
+	
+	@Test
+	public void testServiceRunning() {
+		Boolean b = resource().path(ServiceConfiguration.SVC_INFO)
+							  .accept(MediaType.APPLICATION_JSON)
+							  .get(Boolean.class);
+
+		assertEquals(true, b);
+	}
+	
+}
