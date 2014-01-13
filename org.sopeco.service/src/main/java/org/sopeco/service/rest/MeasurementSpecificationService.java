077afda3ad052da5476461ba12d76e604d027cd9
Peter Merkert <p.merkert@sap.com>
2014-01-13 14:21:11 +0100

    Added two more RESTful services for the measurement specification service.
    Added a creation for measurement specifications and renaming working
    specifications.
    
    Change-Id: I416e70441667362e2de879a819540f71973315e1
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/main/java/org/sopeco/service/rest/MeasurementSpecificationService.java
+++ org.sopeco.service/src/main/java/org/sopeco/service/rest/MeasurementSpecificationService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,9 +13,13 @@ import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sopeco.persistence.IPersistenceProvider;
 import org.sopeco.persistence.entities.definition.MeasurementSpecification;
+import org.sopeco.persistence.entities.definition.ScenarioDefinition;
+import org.sopeco.service.builder.MeasurementSpecificationBuilder;
 import org.sopeco.service.configuration.ServiceConfiguration;
 import org.sopeco.service.persistence.ServicePersistenceProvider;
+import org.sopeco.service.persistence.UserPersistenceProvider;
 import org.sopeco.service.persistence.entities.Users;
 
 @Path(ServiceConfiguration.SVC_MEASUREMENT)
@@ -73,10 +78,85 @@ public class MeasurementSpecificationService {
 		return true;
 	}
 	
+	@POST
+	@Path(ServiceConfiguration.SVC_MEASUREMENT_CREATE)
+	@Produces(MediaType.APPLICATION_JSON)
+	public boolean createSpecification(@QueryParam("token") String usertoken,
+									   @QueryParam("specname") String specificationName) {
+
+		Users u = ServicePersistenceProvider.getInstance().loadUser(usertoken);
+		
+		System.out.println("3Scenario Name: " + u.getCurrentScenarioDefinitionBuilder().getBuiltScenario().getScenarioName());
+		System.out.println("3MeasurementSpec 0th Name: " + u.getCurrentScenarioDefinitionBuilder().getBuiltScenario().getMeasurementSpecifications().get(0).getName());
+		
+		
+		if (existSpecification(specificationName, u)) {
+			LOGGER.warn("Specification with the name '{}' already exists.", specificationName);
+			return false;
+		}
+
+		MeasurementSpecificationBuilder msb = u.getCurrentScenarioDefinitionBuilder()
+													  .addNewMeasurementSpecification();
+		if (msb == null) {
+			LOGGER.warn("Error adding new specification with name '{}'", specificationName);
+			return false;
+		}
+
+		msb.setName(specificationName);
+		
+		System.out.println("Name: " + msb.getBuiltSpecification().getName());
+		
+		// store the scenario definition in the databse of the current user
+		IPersistenceProvider dbCon = UserPersistenceProvider.createPersistenceProvider(usertoken);
+		
+		if (dbCon == null) {
+			LOGGER.warn("No database connection found.");
+			return false;
+		} else {
+			System.out.println("Scenario Name: " + u.getCurrentScenarioDefinitionBuilder().getBuiltScenario().getScenarioName());
+			System.out.println("MeasurementSpec 1th Name: " + u.getCurrentScenarioDefinitionBuilder().getBuiltScenario().getMeasurementSpecifications().get(1).getName());
+			ScenarioDefinition scenarioDef = u.getCurrentScenarioDefinitionBuilder().getBuiltScenario();
+			dbCon.store(scenarioDef);
+		}
+		
+		// save the selected specification in the service-db
+		ServicePersistenceProvider.getInstance().storeUser(u);
+
+		return true;
+	}
 	
-	
-	
-	
+	@PUT
+	@Path(ServiceConfiguration.SVC_MEASUREMENT_RENAME)
+	@Produces(MediaType.APPLICATION_JSON)
+	public boolean renameWorkingSpecification(@QueryParam("token") String usertoken,
+											  @QueryParam("specname") String specname) {
+		
+		Users u = ServicePersistenceProvider.getInstance().loadUser(usertoken);
+		
+		if (existSpecification(specname, u)) {
+			LOGGER.warn("Can't rename, because specification with the name '{}' already exists.", specname);
+			return false;
+		}
+
+		u.getCurrentScenarioDefinitionBuilder().getSpecificationBuilder().setName(specname);
+		
+
+		// store the scenario definition in the databse of the current user
+		IPersistenceProvider dbCon = UserPersistenceProvider.createPersistenceProvider(usertoken);
+		
+		if (dbCon == null) {
+			LOGGER.warn("No database connection found.");
+			return false;
+		} else {
+			ScenarioDefinition scenarioDef = u.getCurrentScenarioDefinitionBuilder().getBuiltScenario();
+			dbCon.store(scenarioDef);
+		}
+
+		// store the new user data in it's database
+		ServicePersistenceProvider.getInstance().storeUser(u);
+
+		return true;
+	}
 	
 	
 	
