67be91998b64c44f3bdc65a515e8a0cb54a2f354
Peter Merkert <p.merkert@sap.com>
2014-01-28 09:37:31 +0100

    Added a comment to the ScheduledExperimentMixIn class.
    Change-Id: I3c602f4ca692f629fa03c152cf360fd2a9393ff7
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/main/java/org/sopeco/service/rest/json/ScheduledExperimentMixIn.java
+++ org.sopeco.service/src/main/java/org/sopeco/service/rest/json/ScheduledExperimentMixIn.java
@@ -4,11 +4,22 @@ import org.sopeco.config.IConfiguration;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+/**
+ * This class' only task is to provide a JSON annotation for a set method.
+ * The method setProperties(IConfiguration configuration) was the default
+ * setter method in the class {@code ScheduledExperiment}. Jackson tried to
+ * use the setter with instantiating an object of interface type
+ * {@code IConfiguration} which led to a Java exception.<br />
+ * To tell Jackson, to ignore this method the annotation is used. As it is not
+ * possible to manipulate the original {@code ScheduledExperiment} class
+ * this MixIn is used.
+ * 
+ * @author Peter Merkert
+ */
 public final class ScheduledExperimentMixIn {
 
 	@JsonIgnore
 	public void setProperties(IConfiguration configuration) {
 	}
-
 	
 }
