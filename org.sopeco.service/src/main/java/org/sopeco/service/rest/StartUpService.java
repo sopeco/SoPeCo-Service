3e2c72d7d32ca3e84dc22aa06832470d6a75fa85
Peter Merkert <p.merkert@sap.com>
2014-01-29 10:22:06 +0100

    Added a startup service to wait for MEController connections with a ServerSocket. The ServerSocket is created on startup of the application.
    Change-Id: Ib1d7068375023fb3f8ca742140a62d46a4704e9c
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

---
+++ org.sopeco.service/src/main/java/org/sopeco/service/rest/StartUpService.java
@@ -0,0 +1,44 @@
+package org.sopeco.service.rest;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.ws.rs.ext.Provider;
+
+import org.sopeco.config.Configuration;
+import org.sopeco.engine.measurementenvironment.socket.SocketAcception;
+import org.sopeco.service.configuration.ServiceConfiguration;
+
+/**
+ * The {@Code StartUpService} class is used to handle the initialization of Servlets.
+ * This class is called once in the program lifetime for the initialization ({@code
+ * contextInitialized}) and once when shutting down ({@code contextDestroyed}).
+ * <br />
+ * <br />
+ * One initialization is the ServerSocket startup for listening for MEControllers.
+ * 
+ * @author Peter Merkert
+ */
+@Provider
+public final class StartUpService implements ServletContextListener {
+
+	/**
+	 * Starts a ServerSocket via the {@code SocketAcception} to wait for
+	 * MEController connection.
+	 */
+	@Override
+    public void contextInitialized(ServletContextEvent sce) {
+    	
+    	// open the ServerSocket on the port set in the configuration
+    	int port = Configuration.getSessionSingleton(Configuration.getGlobalSessionId())
+    						    .getPropertyAsInteger(ServiceConfiguration.MEC_PORT, 11300);
+		if (port > 0) {
+			SocketAcception.open(port);
+		}
+		
+    }
+
+	@Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+    
+}
