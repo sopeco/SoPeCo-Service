696168928d0333bb6d768ff8e1740bfb70227ac4
Peter Merkert <p.merkert@sap.com>
2014-01-30 12:56:56 +0100

    Changed the test MEC from MatrixMultiplicationMEC to TestMEC. The TestMEC is a simple stub for a MEC used in the test environment.
    As the import of the 'colt'-library for math is not used any more, the
    import is removed out of the pom.xml.
    
    Change-Id: Ie0ef7d11f9a8bd7f2e80bbfbf8738ff1f5687499
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

---
+++ org.sopeco.service/src/test/java/org/sopeco/service/test/rest/fake/TestMEC.java
@@ -0,0 +1,98 @@
+package org.sopeco.service.test.rest.fake;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sopeco.engine.measurementenvironment.AbstractMEController;
+import org.sopeco.engine.measurementenvironment.InputParameter;
+import org.sopeco.engine.measurementenvironment.app.MECApplication;
+import org.sopeco.persistence.entities.exceptions.ExperimentFailedException;
+import org.sopeco.service.configuration.ServiceConfiguration;
+import org.sopeco.service.rest.MeasurementControllerService;
+import org.sopeco.service.test.rest.MeasurementControllerServiceTest;
+
+/**
+ * This class is used in the test environment, when the {@link MeasurementControllerService}
+ * is tested in {@link MeasurementControllerServiceTest}.
+ * 
+ * @author Peter Merkert
+ */
+public class TestMEC extends AbstractMEController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestMEC.class);
+	
+	/**
+	 * The MEC_ID represents the ID, with witchever the MEC can be requested on
+	 * the RESTful service.
+	 */
+	public static final String MEC_ID = TestMEC.class.getName();
+	
+	@InputParameter(namespace="test")
+	int abc = 0;
+
+	@Override
+	protected void defineResultSet() {
+	}
+
+	@Override
+	protected void initialize() {
+	}
+
+	@Override
+	protected void prepareExperimentSeries() {
+	}
+
+	@Override
+	protected void runExperiment() throws ExperimentFailedException {
+		System.out.println(1);
+		
+		for (int i = 0; i < abc; i++) {
+			System.out.println(2);
+
+			try {
+
+				print("started");
+
+				Thread.sleep(2500);
+				System.out.println(3);
+
+				print("1");
+				print("2");
+
+				Thread.sleep(2500);
+				System.out.println(4);
+
+				print("ending");
+
+			} catch (Exception e) {
+			}
+
+		}
+		System.out.println(5);
+	}
+
+	@Override
+	protected void finalizeExperimentSeries() {
+	}
+
+	/**
+	 * Starts the TestMEC 3 times with the names "ABC", "Testcontroller"
+	 * and "DasGleicheNochmal".
+	 * <br />
+	 * The socket connection is afterwards established.
+	 * <br />
+	 * Before starting: A ServerSocket must listen on the <code>ServiceConfiguration.MEC_SOCKET_HOST</code>
+	 * and <code>ServiceConfiguration.MEC_SOCKET_PORT</code>.
+	 * <br />
+	 * The TestMEC is registered with the class name ("TestMEC") as identifier.
+	 */
+	public static void start() {
+		LOGGER.debug("Connect the TestMEC controller to the RESTful service ServerSocket.");
+		
+		MECApplication mecapp = MECApplication.get();
+		mecapp.addMeasurementController("ABC", new TestMEC());
+		mecapp.addMeasurementController("Testcontroller", new TestMEC());
+		mecapp.addMeasurementController("DasGleicheNochmal", new TestMEC());
+		mecapp.socketConnect(ServiceConfiguration.MEC_SOCKET_HOST, ServiceConfiguration.MEC_SOCKET_PORT, MEC_ID);
+	}
+
+}
