62e6c6bae98e556e0f8baec69087ea2fb5b543c5
Peter Merkert <p.merkert@sap.com>
2014-01-30 14:07:08 +0100

    Removed unused imprt in ExecutionTest.
    Change-Id: I18a6c9f7ff9f38bd6274630dcb24bd64500f0eb3
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/test/java/org/sopeco/service/test/rest/ExecutionTest.java
+++ org.sopeco.service/src/test/java/org/sopeco/service/test/rest/ExecutionTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.core.MediaType;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
