8ee9a2377aa501c7833165ba9d86e6d9e6c1625c
Peter Merkert <p.merkert@sap.com>
2014-01-30 12:59:22 +0100

    Added the StartUpService to the web.xml to startup on Service startup. The StartUpService starts a ServerSocket when the servlets are created. As the StartUpService is called each time a test case is executed, the socket port is already bind by the first execution. This error is catched and output via a logger warning.
    Change-Id: I492093b0d4e786ac45e854a7de271702d8ed976a
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/main/java/org/sopeco/service/rest/StartUpService.java
+++ org.sopeco.service/src/main/java/org/sopeco/service/rest/StartUpService.java
@@ -4,12 +4,13 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.ws.rs.ext.Provider;
 
-import org.sopeco.config.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sopeco.engine.measurementenvironment.socket.SocketAcception;
 import org.sopeco.service.configuration.ServiceConfiguration;
 
 /**
- * The {@Code StartUpService} class is used to handle the initialization of Servlets.
+ * The <code>StartUpService</code> class is used to handle the initialization of Servlets.
  * This class is called once in the program lifetime for the initialization ({@code
  * contextInitialized}) and once when shutting down ({@code contextDestroyed}).
  * <br />
@@ -21,18 +22,30 @@ import org.sopeco.service.configuration.ServiceConfiguration;
 @Provider
 public final class StartUpService implements ServletContextListener {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(StartUpService.class);
+	
 	/**
-	 * Starts a ServerSocket via the {@code SocketAcception} to wait for
-	 * MEController connection.
+	 * Starts a ServerSocket via the <code>SocketAcception</code> to wait for
+	 * <code>MeasurementEnvironmentController</code> connections.
+	 * <br />
+	 * This is a non-blocking method, because the ServerSocket is handled
+	 * via the class <code>SocketAcception</code>.
 	 */
 	@Override
     public void contextInitialized(ServletContextEvent sce) {
     	
-    	// open the ServerSocket on the port set in the configuration
-		SocketAcception.open(ServiceConfiguration.MEC_SOCKET_PORT);
+		try {
+	    	// open the ServerSocket on the port set in the configuration
+			SocketAcception.open(ServiceConfiguration.MEC_SOCKET_PORT);
+		} catch (RuntimeException re) {
+			LOGGER.warn("Port {} already in use.", ServiceConfiguration.MEC_SOCKET_PORT);
+		}
 		
     }
 
+	/**
+	 * Currently this method is empty overwritten.
+	 */
 	@Override
     public void contextDestroyed(ServletContextEvent sce) {
     }
