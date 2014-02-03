64542fb747f48660f5c66637433ece6f2ca530b4
Peter Merkert <p.merkert@sap.com>
2014-02-03 11:13:04 +0100

    Added comments to all the properties in the TestConfiguration file. It's better to understand for fufuter development.
    Change-Id: I785112efabc620e0f4096ca6875f88338385c3db
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/test/java/org/sopeco/service/test/configuration/TestConfiguration.java
+++ org.sopeco.service/src/test/java/org/sopeco/service/test/configuration/TestConfiguration.java
@@ -7,21 +7,51 @@ package org.sopeco.service.test.configuration;
  */
 public abstract class TestConfiguration {
 
+	/**
+	 * The Jackson package for JSON converting on RESTful interfaces.
+	 */
 	public static final String PACKAGE_NAME_JSON = "org.codehaus.jackson.jaxrs";
+	
+	/**
+	 * The Jersey annotation for POJOMappingFeature.
+	 */
 	public static final String PACKAGE_NAME_POJO = "com.sun.jersey.api.json.POJOMappingFeature";
+	
+	/**
+	 * The package where all the RESTful services are in. Used to setup the {@link JerseyTest}
+	 * correctly.
+	 */
 	public static final String PACKAGE_NAME_REST = "org.sopeco.service.rest";
 
+	/**
+	 * The test account, which the test always wants to connect to.
+	 */
 	public static final String TESTACCOUNTNAME = "testaccount";
+	
+	/**
+	 * The password to access the test account.
+	 */
 	public static final String TESTPASSWORD = "testpassword";
 	
+	/**
+	 * The default test scenario name. It's used when creating scenarios in test cases.
+	 */
 	public static final String TEST_SCENARIO_NAME = "testScenarioName";
 	
+	/**
+	 * The default test measurement specification name. It's used when creating the default scenario.
+	 */
 	public static final String TEST_MEASUREMENT_SPECIFICATION_NAME = "testMeasurementSpecficiationName";
 	
 	/**
-	 * The cleared scenario is always in teh database. It's needed to switch to this scenario and
+	 * The "clearup" scenario is always needed in the database: It's needed to switch to this scenario and
 	 * delete the example scenario everytime.
 	 */
 	public static final String TEST_CLEAN_SCENARIO_NAME = "emptyScenarioName";
+	
+	/**
+	 * The TEST_CLEAN_MEASUREMENT_SPECIFICATION_NAME defines the name for the measurement
+	 * specification for the "cleanup" scenario.
+	 */
 	public static final String TEST_CLEAN_MEASUREMENT_SPECIFICATION_NAME = "emptyMeasurementSpecficiationName";
 }
