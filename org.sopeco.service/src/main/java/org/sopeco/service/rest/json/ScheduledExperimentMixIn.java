b0094490c91e97d8149dae8ce2b80c6acf54b51a
Peter Merkert <p.merkert@sap.com>
2014-02-03 10:21:06 +0100

    Checkstyle cleanup: Added comments to the ScheduledExperimentMixIn.
    Change-Id: Ib025a1949635944bf10220b20bf39accbd644917
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/main/java/org/sopeco/service/rest/json/ScheduledExperimentMixIn.java
+++ org.sopeco.service/src/main/java/org/sopeco/service/rest/json/ScheduledExperimentMixIn.java
@@ -7,17 +7,25 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 /**
  * This class' only task is to provide a JSON annotation for a set method.
  * The method setProperties(IConfiguration configuration) was the default
- * setter method in the class {@code ScheduledExperiment}. Jackson tried to
+ * setter method in the class {@link ScheduledExperiment}. Jackson tried to
  * use the setter with instantiating an object of interface type
- * {@code IConfiguration} which led to a Java exception.<br />
+ * {@link IConfiguration} which led to a Java exception.<br />
  * To tell Jackson, to ignore this method the annotation is used. As it is not
- * possible to manipulate the original {@code ScheduledExperiment} class
+ * possible to manipulate the original <code>ScheduledExperiment</code> class
  * this MixIn is used.
  * 
  * @author Peter Merkert
  */
 public final class ScheduledExperimentMixIn {
 
+	/**
+	 * The setProperties method must be ignore by Json, because otherwise it
+	 * tries to call the method with an object of type {@link IConfiguration}.
+	 * This is going to fail, because <code>IConfiguration</code> is an
+	 * interface and cannot be instantiated.
+	 * 
+	 * @param configuration the configuration
+	 */
 	@JsonIgnore
 	public void setProperties(IConfiguration configuration) {
 	}
