2183de1e216001063ac290fa13d3b9aadf160ec4
Peter Merkert <p.merkert@sap.com>
2014-02-07 12:39:51 +0100

    Removed unused methods from the Crypto module. It's only used to calculate the SHA-256 with Java utilities.
    Change-Id: I9607b1337fdde3781c973958d8fd86835665abdb
    Signed-off-by: Peter Merkert <p.merkert@sap.com>

--- org.sopeco.service/src/main/java/org/sopeco/service/security/Crypto.java
+++ org.sopeco.service/src/main/java/org/sopeco/service/security/Crypto.java
@@ -1,30 +1,20 @@
 package org.sopeco.service.security;
 
-import java.io.UnsupportedEncodingException;
-import java.security.AlgorithmParameters;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.InvalidParameterSpecException;
-import java.security.spec.KeySpec;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-
+/**
+ * Utility class to calculate the SHA-256 of a given string.
+ * 
+ * @author Peter Merkert
+ */
 public final class Crypto {
 
-	/** 8 random bytes */
-	private static final byte[] SALT = { 'S', 'O', 'F', 'T', 'W', 'A', 'R', 'E' };
-
+	private static final int HexOffset = 0xff;
+	
+	/**
+	 * As utility class the constructor is not needed and made private.
+	 */
 	private Crypto() {
 	}
 
@@ -32,90 +22,31 @@ public final class Crypto {
 	 * Calculates the hash value of the given input string using the SHA-256
 	 * algorithm.
 	 * 
-	 * @param input
+	 * @param input the string to hash with SHA-256
 	 * @return hash value of the input string.
 	 */
 	public static String sha256(String input) {
+		
 		try {
 			MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
 			byte[] hashBytes = messageDigest.digest(input.getBytes());
 			StringBuffer hashString = new StringBuffer();
+			
 			for (byte b : hashBytes) {
-				String hex = Integer.toHexString(0xff & b);
-				if (hex.length() == 1)
+				
+				String hex = Integer.toHexString(HexOffset & b);
+				
+				if (hex.length() == 1) {
 					hashString.append(0);
+				}
+				
 				hashString.append(hex);
 			}
+			
 			return hashString.toString();
 		} catch (NoSuchAlgorithmException e) {
 			throw new IllegalStateException(e);
 		}
-	}
-
-	public static byte[][] encrypt(String password, String input) {
-		try {
-			SecretKey secretKey = getSecret(password.toCharArray(), SALT);
-			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-			AlgorithmParameters params = cipher.getParameters();
-
-			byte[][] returnArray = new byte[2][];
-			returnArray[0] = params.getParameterSpec(IvParameterSpec.class).getIV();
-			returnArray[1] = cipher.doFinal(input.getBytes("UTF-8"));
-			return returnArray;
-		} catch (NoSuchAlgorithmException e) {
-			throw new IllegalStateException(e);
-		} catch (NoSuchPaddingException e) {
-			throw new IllegalStateException(e);
-		} catch (InvalidKeyException e) {
-			throw new IllegalStateException(e);
-		} catch (IllegalBlockSizeException e) {
-			throw new IllegalStateException(e);
-		} catch (BadPaddingException e) {
-			throw new IllegalStateException(e);
-		} catch (UnsupportedEncodingException e) {
-			throw new IllegalStateException(e);
-		} catch (InvalidParameterSpecException e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	public static String decrypt(String password, byte[][] inputAndInitialVector) {
-		try {
-			SecretKey secretKey = getSecret(password.toCharArray(), SALT);
-			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(inputAndInitialVector[0]));
-			return new String(cipher.doFinal(inputAndInitialVector[1]), "UTF-8");
-		} catch (NoSuchAlgorithmException e) {
-			throw new IllegalStateException(e);
-		} catch (NoSuchPaddingException e) {
-			throw new IllegalStateException(e);
-		} catch (InvalidKeyException e) {
-			throw new IllegalStateException(e);
-		} catch (UnsupportedEncodingException e) {
-			throw new IllegalStateException(e);
-		} catch (IllegalBlockSizeException e) {
-			throw new IllegalStateException(e);
-		} catch (BadPaddingException e) {
-			throw new IllegalStateException(e);
-		} catch (InvalidAlgorithmParameterException e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	private static SecretKey getSecret(char[] password, byte[] salt) {
-		try {
-			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-			KeySpec keySpec = new PBEKeySpec(password, salt, 65536, 128);
-
-			SecretKey tmpKey = keyFactory.generateSecret(keySpec);
-			SecretKey secret = new SecretKeySpec(tmpKey.getEncoded(), "AES");
-
-			return secret;
-		} catch (NoSuchAlgorithmException e) {
-			throw new IllegalStateException(e);
-		} catch (InvalidKeySpecException e) {
-			throw new IllegalStateException(e);
-		}
+		
 	}
 }
