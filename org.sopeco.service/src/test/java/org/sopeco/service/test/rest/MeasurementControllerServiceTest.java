77e1d1cda895c728ee9f1a9b9350dffe07b13cc2
Neevo Lima <peter.merkert@gmx.de>
2014-02-25 23:14:32 +0100

    Migrated MeasurementControllerServiceTest to work with JAX-RS 2.0
    Change-Id: Id87957a32859dea198a40ee8b8c79019815c9f32
    Signed-off-by: Neevo Lima <peter.merkert@gmx.de>

--- org.sopeco.service/src/test/java/org/sopeco/service/test/rest/MeasurementControllerServiceTest.java
+++ org.sopeco.service/src/test/java/org/sopeco/service/test/rest/MeasurementControllerServiceTest.java
@@ -4,24 +4,32 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
+import javax.validation.constraints.Null;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.After;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sopeco.persistence.entities.definition.ExperimentSeriesDefinition;
 import org.sopeco.service.configuration.ServiceConfiguration;
 import org.sopeco.service.rest.StartUpService;
 import org.sopeco.service.rest.exchange.MECStatus;
-import org.sopeco.service.rest.exchange.ServiceResponse;
-import org.sopeco.service.rest.json.CustomObjectWrapper;
+import org.sopeco.service.rest.json.CustomObjectMapper;
 import org.sopeco.service.test.configuration.TestConfiguration;
 import org.sopeco.service.test.rest.fake.TestMEC;
 
-import com.sun.jersey.api.client.GenericType;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.test.framework.JerseyTest;
-import com.sun.jersey.test.framework.WebAppDescriptor;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+
 
 /**
  * The {@link MeasurementControllerServiceTest} tests the {@link MeasurementControllerService} class.
@@ -45,34 +53,94 @@ public class MeasurementControllerServiceTest extends JerseyTest {
 	public MeasurementControllerServiceTest() {
 		super();
 	}
-
+	
 	/**
-	 * Configure is called on the object creation of a JerseyTest. It's used to
-	 * configure where the JerseyTest can find JSON, the REST service to test
-	 * and the JSON POJO.
-	 * 
-	 * @return the configuration
+	 * This method is called on the Grizzly container creation of a {@link JerseyTest}.
+	 * It's used to configure where the servlet container.<br />
+	 * In this case, the package is definied where the RESTful services are and
+	 * the {@link CustomObjectMapper} is registered.
 	 */
 	@Override
-	public WebAppDescriptor configure() {
-		return new WebAppDescriptor.Builder(TestConfiguration.PACKAGE_NAME_REST)
-				.contextListenerClass(StartUpService.class)
-				.clientConfig(createClientConfig())
-				.build();
-	}
+    protected Application configure() {
+		ResourceConfig rc = new ResourceConfig();
+		rc.packages(TestConfiguration.PACKAGE_NAME_REST);
+		
+		// the CustomObjectMapper must be wrapped into a Jackson Json Provider
+		// otherwise Jersey does not recognize to use Jackson for JSON
+		// converting
+		JacksonJaxbJsonProvider provider = new JacksonJaxbJsonProvider();
+        provider.setMapper(new CustomObjectMapper());
+		rc.register(provider);
+		
+		return rc;
+    }
 
 	/**
-	 * Sets the client config for the client. The method adds a special {@link CustomObjectWrapper}
-	 * to the normal Jackson wrapper for JSON.
-	 * This method is called by {@link configure()}.
-	 * 
-	 * @return ClientConfig to work with JSON
+	 * The {@link Client} needs also the {@link CustomObjectMapper}, which
+	 * defines the mixin used when the objects were serialized.
 	 */
-	private static ClientConfig createClientConfig() {
-		ClientConfig config = new DefaultClientConfig();
-	    // the class contains the configuration to ignore not mappable properties
-	    config.getClasses().add(CustomObjectWrapper.class);
-	    return config;
+	@Override
+	protected void configureClient(ClientConfig config) {
+		JacksonJaxbJsonProvider provider = new JacksonJaxbJsonProvider();
+        provider.setMapper(new CustomObjectMapper());
+        config.register(provider);
+	}
+	
+	/**
+	 * Cleans up the database means: Delete the scenario with name {@code 
+	 * MeasurementControllerServiceTest.SCENARIO_NAME} in the database. This scenario
+	 * is used by every single test and it can cause errors, when the scenario is created,
+	 * but already in the database. Because the database instance is then not updated,
+	 * which can result in unexpected behaviour.
+	 */
+	@After
+	public void cleanUpDatabase() {
+		LOGGER.debug("Cleaning up the database.");
+		String accountname = TestConfiguration.TESTACCOUNTNAME;
+		String password = TestConfiguration.TESTPASSWORD;
+		String scenarioNameEmpty = TestConfiguration.TEST_CLEAN_SCENARIO_NAME;
+		String measSpecNameEmpty = TestConfiguration.TEST_CLEAN_MEASUREMENT_SPECIFICATION_NAME;
+		
+		Response r = target().path(ServiceConfiguration.SVC_ACCOUNT)
+						     .path(ServiceConfiguration.SVC_ACCOUNT_LOGIN)
+						     .queryParam(ServiceConfiguration.SVCP_ACCOUNT_NAME, accountname)
+						     .queryParam(ServiceConfiguration.SVCP_ACCOUNT_PASSWORD, password)
+					      	 .request(MediaType.APPLICATION_JSON)
+						     .get();
+						
+		String token = r.readEntity(String.class);
+
+		// clean the scheduling list for the user
+		target().path(ServiceConfiguration.SVC_EXECUTE)
+				.path(ServiceConfiguration.SVC_EXECUTE_SCHEDULE)
+				.queryParam(ServiceConfiguration.SVCP_SCENARIO_TOKEN, token)
+				.request(MediaType.APPLICATION_JSON)
+				.delete();
+	
+		// now create empty scenario to delete the test scenario
+		ExperimentSeriesDefinition esd = new ExperimentSeriesDefinition();
+		target().path(ServiceConfiguration.SVC_SCENARIO)
+				.path(ServiceConfiguration.SVC_SCENARIO_ADD)
+				.path(scenarioNameEmpty)
+				.queryParam(ServiceConfiguration.SVCP_SCENARIO_SPECNAME, measSpecNameEmpty)
+				.queryParam(ServiceConfiguration.SVCP_SCENARIO_TOKEN, token)
+				.request(MediaType.APPLICATION_JSON)
+				.post(Entity.entity(esd, MediaType.APPLICATION_JSON));
+		
+		target().path(ServiceConfiguration.SVC_SCENARIO)
+				.path(ServiceConfiguration.SVC_SCENARIO_SWITCH)
+				.path(ServiceConfiguration.SVC_SCENARIO_SWITCH_NAME)
+				.queryParam(ServiceConfiguration.SVCP_SCENARIO_NAME, scenarioNameEmpty)
+				.queryParam(ServiceConfiguration.SVCP_SCENARIO_TOKEN, token)
+				.request(MediaType.APPLICATION_JSON)
+				.put(Entity.entity(Null.class, MediaType.APPLICATION_JSON));
+		
+		// delete the example scenario
+		target().path(ServiceConfiguration.SVC_SCENARIO)
+				.path(TestConfiguration.TEST_SCENARIO_NAME)
+			    .queryParam(ServiceConfiguration.SVCP_SCENARIO_TOKEN, token)
+			    .request(MediaType.APPLICATION_JSON)
+			    .delete();
 	}
 	
 	/**
@@ -91,32 +159,37 @@ public class MeasurementControllerServiceTest extends JerseyTest {
 		String password = TestConfiguration.TESTPASSWORD;
 		
 		// log into the account
-		ServiceResponse<String> sr_m = resource().path(ServiceConfiguration.SVC_ACCOUNT)
-											  	 .path(ServiceConfiguration.SVC_ACCOUNT_LOGIN)
-											  	 .queryParam(ServiceConfiguration.SVCP_ACCOUNT_NAME, accountname)
-											  	 .queryParam(ServiceConfiguration.SVCP_ACCOUNT_PASSWORD, password)
-											  	 .get(new GenericType<ServiceResponse<String>>() { });
-		
-		String token = sr_m.getObject();
+		Response r = target().path(ServiceConfiguration.SVC_ACCOUNT)
+			     .path(ServiceConfiguration.SVC_ACCOUNT_LOGIN)
+			     .queryParam(ServiceConfiguration.SVCP_ACCOUNT_NAME, accountname)
+			     .queryParam(ServiceConfiguration.SVCP_ACCOUNT_PASSWORD, password)
+			     .request(MediaType.APPLICATION_JSON)
+			     .get();
+			
+		String token = r.readEntity(String.class);
 		
 		// connect to a random string
-		ServiceResponse<MECStatus> sr_mecStatus = resource().path(ServiceConfiguration.SVC_MEC)
-														    .path(ServiceConfiguration.SVC_MEC_STATUS)
-														    .queryParam(ServiceConfiguration.SVCP_MEC_TOKEN, token)
-														    .queryParam(ServiceConfiguration.SVCP_MEC_URL, "random")
-														    .get(new GenericType<ServiceResponse<MECStatus>>() { });
+		r = target().path(ServiceConfiguration.SVC_MEC)
+			    	.path(ServiceConfiguration.SVC_MEC_STATUS)
+			    	.queryParam(ServiceConfiguration.SVCP_MEC_TOKEN, token)
+			    	.queryParam(ServiceConfiguration.SVCP_MEC_URL, "random")
+			    	.request(MediaType.APPLICATION_JSON)
+			    	.get();
 					
-		assertEquals(MECStatus.NO_VALID_MEC_URL, sr_mecStatus.getObject().getStatus());
+		MECStatus mecstatus = r.readEntity(MECStatus.class);
+		
+		assertEquals(true, mecstatus != null);
+		assertEquals(MECStatus.NO_VALID_MEC_URL, mecstatus.getStatus());
 	
 		// check if a wrong token fails, too
-		sr_mecStatus = resource().path(ServiceConfiguration.SVC_MEC)
-						      	 .path(ServiceConfiguration.SVC_MEC_STATUS)
-						      	 .queryParam(ServiceConfiguration.SVCP_MEC_TOKEN, "myrandomtoken")
-						      	 .queryParam(ServiceConfiguration.SVCP_MEC_URL, "random")
-						      	 .get(new GenericType<ServiceResponse<MECStatus>>() { });
-		
-		assertEquals(Status.UNAUTHORIZED, sr_mecStatus.getStatus());
-		
+		r = target().path(ServiceConfiguration.SVC_MEC)
+		      	 	.path(ServiceConfiguration.SVC_MEC_STATUS)
+		      	 	.queryParam(ServiceConfiguration.SVCP_MEC_TOKEN, "myrandomtoken")
+		      	 	.queryParam(ServiceConfiguration.SVCP_MEC_URL, "random")
+		      	 	.request(MediaType.APPLICATION_JSON)
+		      	 	.get();
+
+		assertEquals(Status.UNAUTHORIZED.getStatusCode(), r.getStatus());
 	}
 	
 	/**
@@ -137,25 +210,30 @@ public class MeasurementControllerServiceTest extends JerseyTest {
 		String socketURI 	= "socket://" + TestMEC.MEC_ID + "/" + TestMEC.MEC_SUB_ID_1;
 		
 		// log into the account
-		ServiceResponse<String> sr_m = resource().path(ServiceConfiguration.SVC_ACCOUNT)
-											  	 .path(ServiceConfiguration.SVC_ACCOUNT_LOGIN)
-											  	 .queryParam(ServiceConfiguration.SVCP_ACCOUNT_NAME, accountname)
-											  	 .queryParam(ServiceConfiguration.SVCP_ACCOUNT_PASSWORD, password)
-											  	 .get(new GenericType<ServiceResponse<String>>() { });
-		
-		String token = sr_m.getObject();
+		Response r = target().path(ServiceConfiguration.SVC_ACCOUNT)
+			     .path(ServiceConfiguration.SVC_ACCOUNT_LOGIN)
+			     .queryParam(ServiceConfiguration.SVCP_ACCOUNT_NAME, accountname)
+			     .queryParam(ServiceConfiguration.SVCP_ACCOUNT_PASSWORD, password)
+		      	 	.request(MediaType.APPLICATION_JSON)
+			     .get();
+			
+		String token = r.readEntity(String.class);
 
 		// now start the MEC fake, which connects to the ServerSocket created by the RESTful service
 		TestMEC.start();
 		
 		// now the controller status is requested
-		ServiceResponse<MECStatus> sr_mecStatus = resource().path(ServiceConfiguration.SVC_MEC)
-													        .path(ServiceConfiguration.SVC_MEC_STATUS)
-													        .queryParam(ServiceConfiguration.SVCP_MEC_TOKEN, token)
-													        .queryParam(ServiceConfiguration.SVCP_MEC_URL, socketURI)
-													        .get(new GenericType<ServiceResponse<MECStatus>>() { });
+		r = target().path(ServiceConfiguration.SVC_MEC)
+			        .path(ServiceConfiguration.SVC_MEC_STATUS)
+			        .queryParam(ServiceConfiguration.SVCP_MEC_TOKEN, token)
+			        .queryParam(ServiceConfiguration.SVCP_MEC_URL, socketURI)
+		      	 	.request(MediaType.APPLICATION_JSON)
+			        .get();
 		
-		assertEquals(MECStatus.STATUS_ONLINE, sr_mecStatus.getObject().getStatus());
+		MECStatus mecstatus = r.readEntity(MECStatus.class);
+
+		assertEquals(true, mecstatus != null);
+		assertEquals(MECStatus.STATUS_ONLINE, mecstatus.getStatus());
 		
 	}
 	
@@ -175,28 +253,33 @@ public class MeasurementControllerServiceTest extends JerseyTest {
 		String mecID 		= TestMEC.MEC_ID;
 		
 		// log into the account
-		ServiceResponse<String> sr_m = resource().path(ServiceConfiguration.SVC_ACCOUNT)
-											  	 .path(ServiceConfiguration.SVC_ACCOUNT_LOGIN)
-											  	 .queryParam(ServiceConfiguration.SVCP_ACCOUNT_NAME, accountname)
-											  	 .queryParam(ServiceConfiguration.SVCP_ACCOUNT_PASSWORD, password)
-											  	 .get(new GenericType<ServiceResponse<String>>() { });
-		
-		String token = sr_m.getObject();
+		Response r = target().path(ServiceConfiguration.SVC_ACCOUNT)
+			     .path(ServiceConfiguration.SVC_ACCOUNT_LOGIN)
+			     .queryParam(ServiceConfiguration.SVCP_ACCOUNT_NAME, accountname)
+			     .queryParam(ServiceConfiguration.SVCP_ACCOUNT_PASSWORD, password)
+		      	 	.request(MediaType.APPLICATION_JSON)
+			     .get();
+			
+		String token = r.readEntity(String.class);
 
 		// now start the MEC fake, which connects to the ServerSocket created by the RESTful service
 		TestMEC.start();
 		
 		// now the controller status is requested
-		ServiceResponse<List<String>> sr_controllerList = resource().path(ServiceConfiguration.SVC_MEC)
-															        .path(ServiceConfiguration.SVC_MEC_LIST)
-															        .queryParam(ServiceConfiguration.SVCP_MEC_TOKEN, token)
-															        .queryParam(ServiceConfiguration.SVCP_MEC_ID, mecID)
-															        .get(new GenericType<ServiceResponse<List<String>>>() { });
+		r = target().path(ServiceConfiguration.SVC_MEC)
+			        .path(ServiceConfiguration.SVC_MEC_LIST)
+			        .queryParam(ServiceConfiguration.SVCP_MEC_TOKEN, token)
+			        .queryParam(ServiceConfiguration.SVCP_MEC_ID, mecID)
+		      	 	.request(MediaType.APPLICATION_JSON)
+			        .get();
+		
+		List<String> list = r.readEntity(new GenericType<List<String>>() { });
 		
 		// now test for each controller in the MEC
-		assertEquals(true, sr_controllerList.getObject().contains(TestMEC.MEC_SUB_ID_1));
-		assertEquals(true, sr_controllerList.getObject().contains(TestMEC.MEC_SUB_ID_2));
-		assertEquals(true, sr_controllerList.getObject().contains(TestMEC.MEC_SUB_ID_3));
+		assertEquals(true, list != null);
+		assertEquals(true, list.contains(TestMEC.MEC_SUB_ID_1));
+		assertEquals(true, list.contains(TestMEC.MEC_SUB_ID_2));
+		assertEquals(true, list.contains(TestMEC.MEC_SUB_ID_3));
 		
 	}
 }
