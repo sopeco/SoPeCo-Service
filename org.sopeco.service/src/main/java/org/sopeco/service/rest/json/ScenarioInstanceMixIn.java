28493b70f95e03a5bd815b3c8a1ba8c19e90f595
d061465 <p.merkert@sap.com>
2014-04-03 09:51:11 +0200

    Fixed the MixIn for the ScenarioInstance. Removed an method annotation and renamed the field inside SPC Core. However, need a new annotation, because of recursive data structures in the ScenarioInstanceMixIn. Have a look at the class comment for more details.
    Change-Id: I787a56fbdb44ec7e460a17b5e75e93c5dca6035e
    Signed-off-by: d061465 <p.merkert@sap.com>

--- org.sopeco.service/src/main/java/org/sopeco/service/rest/json/ScenarioInstanceMixIn.java
+++ org.sopeco.service/src/main/java/org/sopeco/service/rest/json/ScenarioInstanceMixIn.java
@@ -1,19 +1,22 @@
 package org.sopeco.service.rest.json;
 
-import java.util.List;
-
 import org.sopeco.persistence.entities.ExperimentSeries;
 import org.sopeco.persistence.entities.ScenarioInstance;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 /**
  * This Jackson MixIn class is used in the {@link CustomObjectMapper} to mix some Json annotations
  * into the {@link ScenarioInstance} class. These annotations are needed to (de)serialize the class
- * correctly.
+ * correctly.<br />
+ * As the {@link ScenarioInstance} has a List of {@link ExperimentSeries}, and each {@link ExperimentSeries}
+ * has a {@link ScenarioInstance}, we need for the recusion the {@link JsonIdentityInfo}.
  * 
  * @author Peter Merkert
  */
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id")
 public final class ScenarioInstanceMixIn {
 
 	@JsonIgnore
@@ -26,9 +29,4 @@ public final class ScenarioInstanceMixIn {
 		return "";
 	}
 
-	@JsonIgnore
-	public List<ExperimentSeries> getExperimentSeriesList() {
-		return null;
-	}
-	
 }
