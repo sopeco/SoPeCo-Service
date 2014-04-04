dd1abfc24cf69e9caac5e7bc9dfc2d8a33f191f9
d061465 <p.merkert@sap.com>
2014-04-04 13:43:00 +0200

    Added size and id to JsonProperties in DataSetAggregatedMixIn, as they might be not detected automatically because they are private. Added more methods to JsonIgnore in the file.
    Change-Id: If2919ae17f3441e244a4715efbd0fa7c4cafa664
    Signed-off-by: d061465 <p.merkert@sap.com>

--- org.sopeco.service/src/main/java/org/sopeco/service/rest/json/DataSetAggregatedMixIn.java
+++ org.sopeco.service/src/main/java/org/sopeco/service/rest/json/DataSetAggregatedMixIn.java
@@ -3,6 +3,7 @@ package org.sopeco.service.rest.json;
 import java.util.Collection;
 import java.util.List;
 
+import org.sopeco.persistence.dataset.AbstractDataSetColumn;
 import org.sopeco.persistence.dataset.DataSetAggregated;
 import org.sopeco.persistence.dataset.DataSetRow;
 import org.sopeco.persistence.dataset.ParameterValue;
@@ -12,6 +13,7 @@ import org.sopeco.persistence.exceptions.DataNotFoundException;
 import org.sopeco.persistence.util.ParameterCollection;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * MixIn for {@link DataSetAggregated} class, which has some getXYZ methods,
@@ -21,6 +23,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  */
 public final class DataSetAggregatedMixIn<T> {
 
+	@JsonProperty("size")
+	private int size;
+	
+	@JsonProperty("id")
+	private String id;
+	
 	@JsonIgnore
 	public List<DataSetRow> getRowList() {
 		return null;
@@ -48,6 +56,17 @@ public final class DataSetAggregatedMixIn<T> {
 
 	@JsonIgnore
 	public Collection<ParameterValueList<?>> getObservationParameterValues(ParameterCollection<ParameterValue<?>> inputParameterValues) throws DataNotFoundException {
+		return null;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@JsonIgnore
+	public Collection<AbstractDataSetColumn> getColumns() {
+		return null;
+	}
+	
+	@JsonIgnore
+	public Collection<ParameterDefinition> getParameterDefinitions() {
 		return null;
 	}
 
